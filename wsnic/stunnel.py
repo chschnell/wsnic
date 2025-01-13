716f52ed74c420fdc417da1b87f81a7e6c633aeb
Christian Schnell <cs@rekoba.de>
2025-01-13 18:00:30 +0100

    silence stunnel under Docker

--- wsnic/stunnel.py
+++ wsnic/stunnel.py
@@ -21,10 +21,16 @@ class StunnelProxyServer:
             logger.warning(f'stunnel: file not found, TLS support disabled (Debian: install apt package stunnel)')
             return
 
-        stunnel_foreground = 'yes' if logger.isEnabledFor(logging.DEBUG) else 'quiet'
+        ## build stunnel.conf
+        if self.config.is_docker_env:
+            stunnel_foreground = 'quiet'
+            stunnel_debug = 'crit'
+        else:
+            stunnel_foreground = 'yes' if logger.isEnabledFor(logging.DEBUG) else 'quiet'
+            stunnel_debug = 'err'
         stunnel_conf = [
             f'foreground = {stunnel_foreground}',
-            f'debug = err',
+            f'debug = {stunnel_debug}',
             f'',
             f'[wsnic]',
             f'TIMEOUTclose = 0',
@@ -37,14 +43,15 @@ class StunnelProxyServer:
             stunnel_conf.append(f'key = {self.config.wss_private_key}')
 
         stunnel_conf_txt = '\n'.join(stunnel_conf)
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f'stunnel.conf [{self.stunnel_conf.name}]:' + '\n' + stunnel_conf_txt)
 
+        ## write stunnel.conf to temp file
         self.stunnel_conf = tempfile.NamedTemporaryFile(delete=False)
         self.stunnel_conf.write(stunnel_conf_txt.encode() + b'\n')
         self.stunnel_conf.close()
 
-        if logger.isEnabledFor(logging.DEBUG):
-            logger.debug(f'stunnel.conf [{self.stunnel_conf.name}]:' + '\n' + stunnel_conf_txt)
-
+        ## execute stunnel
         stunnel_cmdline = ['stunnel', self.stunnel_conf.name]
         logger.info(f'start child process: {" ".join(stunnel_cmdline)}')
         self.stunnel_p = subprocess.Popen(stunnel_cmdline)
